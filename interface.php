7d31cf2f861269b7880bfac1c4cec18a62771e41
ellaellen <jinglefarmer@gmail.com>
2015-02-14 17:28:51 -0700

    add table to list the videos in interface.php

--- interface.php
+++ interface.php
@@ -1,4 +1,5 @@
 <?php
+
 //Turn on error reporting
 ini_set('display_errors', 'On');
 //Connects to the database
@@ -11,11 +12,12 @@ if(!$mysqli || $mysqli->connect_errno){
 <!DOCTYPE html >
 
 <html>
-
+<head>
+</head>
 <body>
 
 <div>
-<form method="POST" action="addvideo.php">
+<form method="POST" action = "addvideo.php">
 <fieldset>
             <legend>Add Video</legend>
             <p>Name: <input type = "text" name = "name"/></p>
@@ -23,7 +25,38 @@ if(!$mysqli || $mysqli->connect_errno){
             <p>Length: <input type = "text" name = "length"/></p>
         </fieldset>
 <p><input type = "submit" name = "Add" value = "Add a video"/></p>
-    </form>
+</form>
+</div>
+
+<div id = "video">
+<table>
+    <tr>
+        <td>Name</td>
+        <td>Category</td>
+        <td>Length</td>
+    </tr>
+<?php
+
+$query = "SELECT name, category, length, rented FROM inventory";
+
+if(!($stmt = $mysqli->prepare($query))){
+        echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+if(!$stmt->execute()){
+        echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+}
+if(!$stmt->bind_result($name, $category, $length, $rented)) {
+        echo "Bind failed: " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+
+while ($stmt->fetch()){
+        echo  "<tr>\n<td>\n" . $name . "\n</td>\n<td>\n" . $category . "\n</td>\n<td>\n" . $length ."\n</td>\n<td>\n" . $rented ."\n</td>\n</tr>";
+}
+
+$stmt->close();
+
+?>
+</table>
 </div>
 </body>
 </html>
